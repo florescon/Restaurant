fa836bfecdd02de5d25d5ed1f4dbd00ef628bd99
florescon <florescontrer@gmail.com>
2023-10-22 16:23:00 -0600

    oct 22

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -85,7 +85,7 @@
                             </div>
 
                             <x-jet-dropdown-link href="{{ route('profile.show') }}">
-                                {{ __('Profile') }}
+                                {{ __('Perfil') }}
                             </x-jet-dropdown-link>
 
                             @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
@@ -103,7 +103,7 @@
                                 <x-jet-dropdown-link href="{{ route('logout') }}"
                                          onclick="event.preventDefault();
                                                 this.closest('form').submit();">
-                                    {{ __('Log Out') }}
+                                    {{ __('Cerrar sesión') }}
                                 </x-jet-dropdown-link>
                             </form>
                         </x-slot>
